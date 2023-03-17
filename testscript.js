53c346f0ad50fdd7c71637d2495f6daec914692b
Timo_Bos <t.bos.1998@gmail.com>
2023-03-17 15:06:14 +0100

    Removed console log in testscript

--- testscript.js
+++ testscript.js
@@ -2,7 +2,6 @@ const toastTrigger = document.getElementById('liveToastBtn')
 const toastTrigger2 = document.getElementsByClassName('toastItem')
 const toastLiveExample = document.getElementById('liveToast')
 const toastText = document.getElementById('toast-text')
-console.log(toastTrigger2)
 if (toastTrigger2.length > 0) {
   for (let i = 0; i < toastTrigger2.length; i++) {
     toastTrigger2[i].addEventListener('click', () => {
